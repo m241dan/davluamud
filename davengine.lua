8c0979502f0f11747ca18d7295f66a9ccd1e6f3c
Davenge <korisd@gmail.com>
2015-08-17 16:34:37 -0400

    changed some notes in davengine.lua

--- davengine.lua
+++ davengine.lua
@@ -3,10 +3,10 @@
    The thread manager is the main loop of the program. It processes the data
    from the mud server first. Looking first at new connection threads. Next,
    it runs all the connection threads looking for new sent data. After that,
-   game logic is run, processing the new data sent by the client and
+   game server is run, processing the new data sent by the client and
    returning any output needed. The database is updated on demand since
    there is no chance of concurrency issues.
 --]]
 -- boot mud server
 -- boot database
-
+-- boot game server(game logic)
