87e88dc5484f4f83c444055432bfa2a3ad219ae2
Davenge <korisd@gmail.com>
2015-09-02 11:33:56 -0400

    adding functionality into clients

--- objects/client.lua
+++ objects/client.lua
@@ -25,15 +25,43 @@ function C:new( connect )
    return client;
 end
 
-function C:addState( name, behaviour )
+function C.state:new( name, behaviour )
    local state = {}
+
+   setmetatable( state, self )
+   self.__index = self
+
    state.name = name
    state.behaviour = require( behaviour )
    state.inbuf = {}
    state.outbuf = { Buffer:new( 70 ) }
    assert( state.behaviour.init( client, state ) )
+end
+
+function C:addState( state )
    table.insert( self.states, #self.states + 1, state )
    return #self.states + 1
+end
+
+function C:setState( si )
+   if( type( si ) == "number" ) then
+      if( not self.states[si] ) then
+         error( "attempting to set client state to a nil index.", 2 )
+      end
+   else
+      if( getmetatable( si ) ~= C.state ) then
+         error( "attemptingto set state using object with non-state metatable", 2 )
+      end
+      for i, s in ipairs( self.states ) do
+         if( si == s ) then
+            si = i
+            break
+         end
+      end
+   end
+
+   self.current_state = self.states[si]
+   return true
 end
 
 function 
